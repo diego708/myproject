12fc5823ce0e99c77af47cf8c21d08f0bd136592
souza <souza@pwx.cloud>
2020-04-26 18:52:22 -0300

    Adicionado os arquivos da aplicacao

---
+++ books/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+
+#from . import views
+from books import views
+
+'''
+urlpatterns = [
+    path('', views.BookList.as_view(), name='book_list'),
+    path('view/<int:pk>', views.BookView.as_view(), name='book_view'),
+    path('new', views.BookCreate.as_view(), name='book_new'),
+    path('view/<int:pk>', views.BookView.as_view(), name='book_view'),
+    path('edit/<int:pk>', views.BookUpdate.as_view(), name='book_edit'),
+    path('delete/<int:pk>', views.BookDelete.as_view(), name='book_delete'),
+]
+'''
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('view/<int:pk>', views.book_view, name='book_view'),
+    path('new', views.book_create, name='book_new'),
+    path('edit/<int:pk>', views.book_update, name='book_edit'),
+    path('delete/<int:pk>', views.book_delete, name='book_delete'),
+]
---
+++ readme.txt
@@ -0,0 +1,15 @@
+
+Exemplo de uma plicação em Django.
+Crud simples para um cadastro de livro.
+
+django-admin startproject my_proj
+
+python manage.py startapp books
+
+Executar os comandos para criar o banco de dados:
+python manage.py makemigrations
+python manage.py migrate
+
+python manage.py runserver
+http://localhost:8000/books/
+
